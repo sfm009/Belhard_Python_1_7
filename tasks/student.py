eebb13ccc48f6dae36b41e29ac0f935432d437f7
unknown <sfm009@gmail.com>
2022-08-15 17:14:50 +0300

    student

--- tasks/student.py
+++ tasks/student.py
@@ -19,3 +19,39 @@
 
 Вывести студентов, у которых средний балл больше 5
 """
+
+
+class Student:
+    surname = 'фамилия'
+    name = 'имя'
+    group = 'номер группы'
+    average_score = 'средний балл'
+
+    def __init__(self, surname, name, group, average_score):
+        self.surname = surname
+        self.name = name
+        self.group = group
+        self.average_score = average_score
+
+    def __eq__(self, other):
+        return self.average_score == other
+
+    def __ne__(self, other):
+        return self.average_score != other
+
+    def __lt__(self, other):
+        return self.average_score < other
+
+    def __gt__(self, other):
+        return self.average_score > other
+
+    def __le__(self, other):
+        return self.average_score <= other
+
+    def __ge__(self, other):
+        return self.average_score >= other
+
+
+students_list = []
+
+

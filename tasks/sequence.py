c558b094216f5a92449b2969335a4a476adb7107
unknown <sfm009@gmail.com>
2022-08-15 17:57:26 +0300

    sequence

--- tasks/sequence.py
+++ tasks/sequence.py
@@ -16,3 +16,20 @@
     random.randint(-n, n)
 - метод print_seq, который выводит последовательность на экран
 """
+from random import random
+
+
+class RandSequence:
+
+    def __init__(self, n, sequence):
+        self.n = n
+        self.sequence = sequence
+
+    def generate(self, n):
+        self.n = n
+        self.sequence = random.randit(-self.n, self.n)
+
+    def print_seq(self):
+        print(self.sequence)
+
+new_obj = RandSequence()
